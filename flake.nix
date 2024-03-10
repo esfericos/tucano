106ffa0bfd59c4978d95601cf897a9af0e86c97b
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2024-03-10 11:03:06 -0300

    Initial workspace setup; add nix

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+      inherit (pkgs) lib;
+      inherit (pkgs.stdenv) isDarwin;
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "tucano";
+        packages = let
+          commonPackages = with pkgs; [
+            # The usual Rust profile
+            (rust-bin.stable."1.76.0".default.override {
+              extensions = ["rust-src" "rust-analyzer" "llvm-tools"];
+            })
+            # We need a nightly version of rustfmt to format this crate
+            (writeShellApplication {
+              name = "cargo-nightly-fmt";
+              runtimeInputs = [
+                (rust-bin.selectLatestNightlyWith (toolchain:
+                  toolchain.minimal.override {
+                    extensions = ["rustfmt"];
+                  }))
+              ];
+              text = ''cargo fmt "$@"'';
+            })
+            cargo-nextest
+            cargo-llvm-cov
+
+            gnumake
+
+            pkg-config
+            llvmPackages_16.llvm
+            llvmPackages_16.bintools
+            libiconv
+          ];
+          darwinPackages = lib.optionals isDarwin (with pkgs.darwin.apple_sdk.frameworks; [
+            CoreFoundation
+            CoreServices
+            SystemConfiguration
+          ]);
+        in (commonPackages ++ darwinPackages);
+      };
+    });
+}
