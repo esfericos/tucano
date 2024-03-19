5de7f17a8d18c482843b9ee87faf18ce2578d1c9
Yuri <49888082+YuriRDev@users.noreply.github.com>
2024-03-18 21:19:32 -0300

    feat(worker/monitor): Free memory

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -1,3 +1,13 @@
+use sysinfo::{    Components, Disks, Networks, System,};
+
 fn main() {
-    println!("Hello, world!");
+  get_memory();
+}
+
+fn get_memory() {
+  let mut system = System::new_all();
+  system.refresh_all();
+  
+  let free_memory = (system.total_memory() - system.used_memory()) as f64;
+  println!("{:.2}", free_memory / 1024.0 / 1024.0 / 1024.0);
 }
