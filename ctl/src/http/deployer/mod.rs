2119ebec62fb2fa00d23c2dbf6a2a98dd0c2a20b
Yuri <49888082+YuriRDev@users.noreply.github.com>
2024-04-22 22:19:54 -0300

    feat(deployer) - Added return method

--- ctl/src/http/deployer/mod.rs
+++ ctl/src/http/deployer/mod.rs
@@ -1,8 +1,24 @@
 use axum::{extract::State, Json};
-use proto::{common::node::Metrics, ctl::deployer::DeployReq};
+use proto::{
+    common::node::Metrics,
+    ctl::deployer::{DeployId, DeployReq, DeployRes, RevisionId},
+};
+use uuid::Uuid;
 
 use crate::discovery::DiscoveryHandle;
 
-pub async fn deploy(State(state): State<DiscoveryHandle>, Json(payload): Json<DeployReq>) {
+pub async fn deploy(
+    State(discovery_handle): State<DiscoveryHandle>,
+    Json(payload): Json<DeployReq>,
+) -> Json<DeployRes> {
     println!("{payload:#?}");
+
+    let revision_id = RevisionId::now_v7();
+
+    discovery_handle.schedule_deploy(revision_id);
+
+    Json(DeployRes {
+        revision_id,
+        deploy_ids: Vec::from([DeployId::now_v7()]),
+    })
 }
