78409b6141fb89e5d3f5f09fc7be5bc7caf8657a
Gustavo Dias de Aguiar <79119534+gustavodiasag@users.noreply.github.com>
2024-05-31 11:59:39 -0300

    feat(cli): initial structure (#40)
    Co-authored-by: Eduardo Lemos <eduardolemosep@gmail.com>
    Co-authored-by: Luiz Felipe Gonçalves <git@luizfelipe.dev>

---
+++ cli/src/main.rs
@@ -0,0 +1,66 @@
+use std::net::SocketAddr;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+pub struct Cli {
+    #[command(subcommand)]
+    cmd: Cmd,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Cmd {
+    #[clap(subcommand)]
+    Node(NodeCmd),
+    #[clap(subcommand)]
+    Service(ServiceCmd),
+}
+
+#[derive(Debug, Subcommand)]
+pub enum NodeCmd {
+    List,
+    Show {
+        address: SocketAddr,
+    },
+    #[clap(subcommand)]
+    Worker(WorkerCmd),
+}
+
+#[derive(Debug, Subcommand)]
+pub enum WorkerCmd {
+    Remove { address: SocketAddr },
+}
+
+#[derive(Debug, Subcommand)]
+pub enum ServiceCmd {
+    List,
+    Show { id: String },
+    Deploy { id: String, image: String },
+    Terminate { id: String },
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match cli.cmd {
+        Cmd::Node(cmd) => handle_node(&cmd),
+        Cmd::Service(cmd) => handle_service(&cmd),
+    }
+}
+
+fn handle_node(cmd: &NodeCmd) {
+    match cmd {
+        NodeCmd::List => todo!(),
+        NodeCmd::Show { .. } => todo!(),
+        NodeCmd::Worker(_) => todo!(),
+    }
+}
+
+fn handle_service(cmd: &ServiceCmd) {
+    match cmd {
+        ServiceCmd::List => todo!(),
+        ServiceCmd::Show { .. } => todo!(),
+        ServiceCmd::Deploy { .. } => todo!(),
+        ServiceCmd::Terminate { .. } => todo!(),
+    }
+}
