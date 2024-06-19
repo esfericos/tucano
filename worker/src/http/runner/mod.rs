74bf65a2d43e54c7b25fd59fccabf972ebe36f7e
Eduardo Lemos <eduardolemosep@gmail.com>
2024-06-19 14:56:52 -0300

    added response status code

--- worker/src/http/runner/mod.rs
+++ worker/src/http/runner/mod.rs
@@ -1,5 +1,6 @@
-use axum::{extract::State, Json};
+use axum::{extract::State, response::IntoResponse, Json};
 use proto::worker::runner::{DeployInstanceReq, DeployInstanceRes, TerminateReq, TerminateRes};
+use reqwest::StatusCode;
 use setup::http;
 
 use crate::http::HttpState;
@@ -7,15 +8,15 @@ use crate::http::HttpState;
 pub async fn deploy_instance(
     State(state): State<HttpState>,
     Json(payload): Json<DeployInstanceReq>,
-) -> http::Result<Json<DeployInstanceRes>> {
+) -> http::Result<impl IntoResponse> {
     state.runner.deploy_instance(payload.instance_spec).await?;
-    Ok(Json(DeployInstanceRes {}))
+    Ok((StatusCode::ACCEPTED, Json(DeployInstanceRes {})))
 }
 
 pub async fn terminate_instance(
     State(state): State<HttpState>,
     Json(payload): Json<TerminateReq>,
-) -> http::Result<Json<TerminateRes>> {
+) -> http::Result<impl IntoResponse> {
     state.runner.terminate_instance(payload.instance_id).await?;
-    Ok(Json(TerminateRes {}))
+    Ok((StatusCode::ACCEPTED, Json(TerminateRes {})))
 }
