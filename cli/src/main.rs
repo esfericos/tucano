9bd3ace054ac4c5c655fc2888dea9decfe60bfff
Eduardo Lemos <eduardolemosep@gmail.com>
2024-06-20 20:15:47 -0300

    resolved comments

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,7 @@
 use std::net::IpAddr;
 
 use clap::{Parser, Subcommand};
+use eyre::Ok;
 use prettytable::{cell::Cell, row::Row, Table};
 use proto::clients::CtlClient;
 
@@ -44,28 +45,31 @@ pub enum ServiceCmd {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> eyre::Result<()> {
     let cli = Cli::parse();
     let ctl_client = CtlClient::new(cli.ctl_addr);
 
     match cli.cmd {
-        Cmd::Node(cmd) => handle_node(&cmd, ctl_client).await,
-        Cmd::Service(cmd) => handle_service(&cmd),
+        Cmd::Node(cmd) => handle_node(&cmd, ctl_client).await?,
+        Cmd::Service(cmd) => handle_service(&cmd)?,
     }
+
+    Ok(())
 }
 
-async fn handle_node(cmd: &NodeCmd, ctl_client: CtlClient) {
+async fn handle_node(cmd: &NodeCmd, ctl_client: CtlClient) -> eyre::Result<()> {
     match cmd {
         NodeCmd::List => {
             let workers = ctl_client.query_workers().await.unwrap().workers;
-            build_table(workers);
+            print_table(workers);
+            Ok(())
         }
         NodeCmd::Show { .. } => todo!(),
         NodeCmd::Worker(_) => todo!(),
     }
 }
 
-fn handle_service(cmd: &ServiceCmd) {
+fn handle_service(cmd: &ServiceCmd) -> eyre::Result<()> {
     match cmd {
         ServiceCmd::List => todo!(),
         ServiceCmd::Show { .. } => todo!(),
@@ -74,15 +78,13 @@ fn handle_service(cmd: &ServiceCmd) {
     }
 }
 
-fn build_table(workers: Vec<IpAddr>) {
+fn print_table(workers: Vec<IpAddr>) {
     let mut table = Table::new();
-    let mut count = 1;
-    for addr in workers {
+    for (i, addr) in workers.into_iter().enumerate() {
         table.add_row(Row::new(vec![
-            Cell::new(format!("worker_{count}").as_str()),
+            Cell::new(format!("worker_{i}").as_str()),
             Cell::new(format!("{addr}").as_str()),
         ]));
-        count += 1;
     }
     table.printstd();
 }
